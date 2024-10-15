399bcc8de310855ec8e9318b382ca1a8e9649ecd
Marceline Kelly <34078276+mckel5@users.noreply.github.com>
2024-10-15 20:34:21 +0000

    handle cases where zero or one SLA entries exist on device

--- server/monitor.py
+++ server/monitor.py
@@ -6,91 +6,103 @@ from pymongo import MongoClient
 from datetime import datetime
 import re
 
-load_dotenv()
 
-db_client = MongoClient()
-db = db_client.netmap
+def main():
+    load_dotenv()
 
-# Clear the statistics table
-db.sla_stats.delete_many({})
+    db_client = MongoClient()
+    db = db_client.netmap
 
-for node in db.nodes.find():
-    device = {
-        "device_type": "cisco_ios",
-        "ip": node["ip"],
-        "username": getenv("MONITOR_USERNAME"),
-        "password": getenv("MONITOR_PASSWORD"),
+    # Clear the statistics table
+    db.sla_stats.delete_many({})
+
+    for node in db.nodes.find():
+        device = {
+            "device_type": "cisco_ios",
+            "ip": node["ip"],
+            "username": getenv("MONITOR_USERNAME"),
+            "password": getenv("MONITOR_PASSWORD"),
+        }
+
+        with netmiko.ConnectHandler(**device) as connection:
+            try:
+                sla_configuration = json.loads(
+                    connection.send_command("show ip sla configuration | json")
+                )["TABLE_configuration"]["ROW_configuration"]
+            except json.JSONDecodeError:
+                # SLA is not configured on this device
+                continue
+
+            # Device will return a single object if only one entry exists
+            if not isinstance(sla_configuration, list):
+                sla_configuration = [sla_configuration]
+
+            parsed_stats = format_sla_stats(sla_configuration, connection, device["ip"])
+            db.sla_stats.insert_many(parsed_stats)
+
+
+def format_sla_stats(
+    sla_configuration: list[dict[str, str]],
+    connection: netmiko.ConnectHandler,
+    source_address: str,
+) -> list[dict[str, any]]:
+    result = []
+
+    DESIRED_STATISTICS = {
+        "ds-jitter-avg": "int",
+        "ds-jitter-max": "int",
+        "ds-jitter-min": "int",
+        "ds-lat-ow-avg": "int",
+        "ds-lat-ow-max": "int",
+        "ds-lat-ow-min": "int",
+        "latest-start-time": "date",
+        "pkt-loss-ds": "int",
+        "pkt-loss-sd": "int",
+        "rtt-avg": "int",
+        "rtt-max": "int",
+        "rtt-min": "int",
+        "sd-jitter-avg": "int",
+        "sd-jitter-max": "int",
+        "sd-jitter-min": "int",
+        "sd-lat-ow-avg": "int",
+        "sd-lat-ow-max": "int",
+        "sd-lat-ow-min": "int",
     }
 
-    with netmiko.ConnectHandler(**device) as connection:
-        sla_entries = json.loads(
-            connection.send_command("show ip sla configuration | json")
-        )["TABLE_configuration"]["ROW_configuration"]
+    for entry in sla_configuration:
+        index = entry["index"]
+        source_address = source_address
+        target_address = entry["dest-ip"]
 
-        for entry in sla_entries:
-            index = entry["index"]
-            source_address = device["ip"]
-            target_address = entry["dest-ip"]
+        sla_result = json.loads(
+            connection.send_command(f"show ip sla statistics {index} | json")
+        )["TABLE_stats"]["ROW_stats"]["TABLE_detail"]["ROW_detail"]
 
-            sla_stats = json.loads(
-                connection.send_command(f"show ip sla statistics {index} | json")
-            )["TABLE_stats"]["ROW_stats"]["TABLE_detail"]["ROW_detail"]
+        formatted_result = {}
 
-            # desired_keys: dict[str, type] = {
-            #     "ds-jitter-avg": int,
-            #     "ds-jitter-max": int,
-            #     "ds-jitter-min": int,
-            #     "ds-lat-ow-avg": int,
-            #     "ds-lat-ow-max": int,
-            #     "ds-lat-ow-min": int,
-            #     "latest-start-time": datetime,
-            #     "pkt-loss-ds": int,
-            #     "pkt-loss-sd": int,
-            #     "rtt-avg": int,
-            #     "rtt-max": int,
-            #     "rtt-min": int,
-            #     "sd-jitter-avg": int,
-            #     "sd-jitter-max": int,
-            #     "sd-jitter-min": int,
-            #     "sd-lat-ow-avg": int,
-            #     "sd-lat-ow-max": int,
-            #     "sd-lat-ow-min": int,
-            # }
-
-            # Filter out unnecessary data, converting values to proper data type if applicable
-            # document = {k: sla_stats[k] for k in desired_keys}
-            # document = {
-            #     k: int(v) if desired_keys[k] is int else v for k, v in document.items()
-            # }
-            # document = dict()
-
-            # for key, type in desired_keys.items():
-            #     if type is int:
-            #         document[key] = int(sla_stats[key])
-            #     if type is datetime:
-            #         document[key] = datetime.strptime(
-            #             sla_stats[key], "%H:%M:%S.%f %Z %a %b %d %Y"
-            #         )
-
-            for stat_name in sla_stats:
-                if stat_name == "latest-start-time":
-                    sla_stats[stat_name] = datetime.strptime(
-                        sla_stats[stat_name], "%H:%M:%S.%f %Z %a %b %d %Y"
+        # Convert data to proper types, if needed
+        for statistic, datatype in DESIRED_STATISTICS.items():
+            match datatype:
+                case "int":
+                    formatted_result[statistic] = int(sla_result[statistic])
+                case "date":
+                    formatted_result[statistic] = datetime.strptime(
+                        sla_result[statistic], "%H:%M:%S.%f %Z %a %b %d %Y"
                     )
-                    continue
 
-                sla_stats[stat_name] = int(sla_stats[stat_name])
+        formatted_result["src-ip"] = source_address
+        formatted_result["dest-ip"] = target_address
+        formatted_result["successes"] = int(
+            re.search("\d+", sla_result["outstring1"]).group()
+        )
+        formatted_result["failures"] = int(
+            re.search("\d+", sla_result["outstring2"]).group()
+        )
+
+        result.append(formatted_result)
+
+    return result
 
 
-
-            # '%H:%M:%S.%f %Z %a %b %d %Y'
-
-            # # Convert values to integers, if applicable
-            # document = {key: int(document[key]) if document[key].isdecimal() else document[key] for key in document.keys()}
-
-            sla_stats["src-ip"] = source_address
-            sla_stats["dest-ip"] = target_address
-            sla_stats["successes"] = int(re.search("\d+", sla_stats["outstring1"]))
-            sla_stats["failures"] = int(re.search("\d+", sla_stats["outstring2"]))
-
-            db.sla_stats.insert_one(sla_stats)
+if __name__ == "__main__":
+    main()
