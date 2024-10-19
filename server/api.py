dd59e6761e3953ad5519affc48d2268ae24227d1
Marceline Kelly <34078276+mckel5@users.noreply.github.com>
2024-10-19 16:43:48 +0000

    update API routes

--- server/api.py
+++ server/api.py
@@ -1,23 +1,32 @@
 from flask import Flask, Response
+from flask_cors import CORS
 from pymongo import MongoClient
 from bson import json_util
 
 app = Flask(__name__)
+CORS(app)
 
 db_client = MongoClient()
 db = db_client.netmap
-nodes = db.node
+nodes = db.nodes
 sla_stats = db.sla_stats
 
 
-@app.route("/api/source/<address>")
-def source(address: str):
+@app.route("/api/node/<hostname>/source")
+def source(hostname: str):
+    ip = nodes.find_one(filter={"hostname": hostname})["ip"]
+
     return Response(
-        json_util.dumps(sla_stats.find(filter={"src-ip": address}).to_list()), mimetype="application/json"
+        json_util.dumps(sla_stats.find(filter={"src-ip": ip}).to_list()),
+        mimetype="application/json",
     )
 
-@app.route("/api/dest/<address>")
-def dest(address: str):
+
+@app.route("/api/node/<hostname>/target")
+def target(hostname: str):
+    ip = nodes.find_one(filter={"hostname": hostname})["ip"]
+
     return Response(
-        json_util.dumps(sla_stats.find(filter={"dest-ip": address}).to_list()), mimetype="application/json"
+        json_util.dumps(sla_stats.find(filter={"dest-ip": ip}).to_list()),
+        mimetype="application/json",
     )
