193e7e891458be7bc394d4416783dd0d1a8ea4fa
Marceline Kelly <34078276+mckel5@users.noreply.github.com>
2024-10-23 13:48:20 +0000

    create rudimentary node map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { ReactFlow } from '@xyflow/react';
+import React from 'react';
+import NodeMap from './NodeMap';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import '@xyflow/react/dist/style.css';
-import NodeMap from './NodeMap';
-
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
-
 export default function App() {
-  // return (
-  //   <div style={{ width: '100vw', height: '100vh' }}>
-  //     <ReactFlow nodes={initialNodes} edges={initialEdges} />
-  //   </div>
-  // );
-  // const [data, setData] = useState([]);
-
-  // useEffect(() => {
-  //   fetch('/api/data')
-  //     .then(response => response.json())
-  //     .then(data => setData(data));
-  // }, []);
-
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='nodes/:hostname' element={<NodeMap />}>
+          <Route path='node/:hostname' element={<NodeMap />}>
           </Route>
         </Routes>
       </BrowserRouter>
--- client/src/NodeMap.jsx
+++ client/src/NodeMap.jsx
@@ -1,8 +1,84 @@
-import React from 'react';
+import { React, useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
+import { ReactFlow, Controls, Background, applyEdgeChanges, applyNodeChanges } from '@xyflow/react';
+
+import '@xyflow/react/dist/style.css';
+
+// export default function NodeMap() {
+//   return <p>Hello</p>;
+// }
 
 export default function NodeMap() {
-  const { hostname } = useParams();
+  const { hostname: sourceNodeName } = useParams();
+  const url = new URL('http://10.10.96.234:5000/api/stats');
+  url.searchParams.set('source', sourceNodeName);
 
-  return <h2>{hostname}</h2>;
+  // const [data, setData] = useState([]);
+  const [nodes, setNodes] = useState([]);
+  const [edges, setEdges] = useState([]);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [],
+  );
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+
+        let nodes = [];
+        let edges = [];
+
+        nodes.push({ id: sourceNodeName, position: { x: 0, y: 0 }, data: { label: sourceNodeName } });
+
+        // Target nodes are evenly distributed in a circle around the source node
+        const mapRadius = Math.min(innerWidth, innerHeight) * 0.5;
+        const numOuterNodes = result.length;
+
+        for (const [i, statistic] of result.entries()) {
+          // Use target node's hostname if it is known, otherwise use its IP address
+          const targetIp = statistic['dest-ip'];
+          const targetNode = await (await fetch(`http://10.10.96.234:5000/api/nodes?ip=${targetIp}`)).json();
+          const targetNodeName = targetNode ? targetNode.hostname : targetIp;
+
+          // Calculate target node's position on circle
+          const x = mapRadius * Math.cos((i + 1) * ((2 * Math.PI) / numOuterNodes));
+          const y = mapRadius * Math.sin((i + 1) * ((2 * Math.PI) / numOuterNodes));
+
+          nodes.push({ id: targetNodeName, position: { x: x, y: y }, data: { label: targetNodeName } });
+          edges.push({ id: i.toString(), source: sourceNodeName, target: targetNodeName })
+        }
+
+        setNodes(nodes);
+        setEdges(edges);
+
+        // setInitialEdges([{ id: 'e1-2', source: '1', target: '2' }]);
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    })();
+  }, []);
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        fitView
+        colorMode="dark"
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 }
